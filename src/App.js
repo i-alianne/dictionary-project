336d0e00bad0b3ed57275216c004259edb6dc68c
Alianne Dosal Allison <adosalallison@gmail.com>
2024-10-09 12:18:13 +0200

    Setup search engine

--- src/App.js
+++ src/App.js
@@ -1,14 +1,19 @@
 import logo from "./logo.png";
+import Dictionary from "./Dictionary";
 import "./App.css";
 
-function App() {
+export default function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo img-fluid" alt="logo" />
-      </header>
+      <div className="container">
+        <header className="App-header">
+          <img src={logo} className="App-logo img-fluid" alt="logo" />
+        </header>
+        <main>
+          <Dictionary />
+        </main>
+        <footer className="App-footer">Coded with 💜 by Alianne</footer>
+      </div>
     </div>
   );
 }
-
-export default App;
