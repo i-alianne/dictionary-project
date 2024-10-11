82c0e06f6a1ce3b0dd4ea93edd964635aa4b3b53
Alianne Dosal Allison <adosalallison@gmail.com>
2024-10-11 12:01:14 +0200

    Added dictionary API integration

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import logo from "./logo.png";
-import Dictionary from "./Dictionary";
+import Dictionary from "./Dictionary.js";
 import "./App.css";
 
 export default function App() {
--- src/Dictionary.js
+++ src/Dictionary.js
@@ -1,12 +1,21 @@
 import React, { useState } from "react";
+import axios from "axios";
 import "./Dictionary.css";
 
 export default function Dictionary() {
   let [keyword, setKeyword] = useState("");
 
+  function handleResponse(response) {
+    console.log(response.data);
+  }
+
   function search(event) {
     event.preventDefault();
-    alert(`Searching for ${keyword}`);
+
+    let apiKey = "be427b594372a38b6f2t7o035dc1e24d";
+    let apiUrl = `https://api.shecodes.io/dictionary/v1/define?word=${keyword}&key=${apiKey}`;
+
+    axios.get(apiUrl).then(handleResponse);
   }
 
   function handleKeywordChange(event) {
